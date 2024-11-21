1e892084bafcd745f83a9eb0fc141c961eadf584
rembertoS <rsilv.fiu.edu>
2024-11-21 13:23:13 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,29 @@
+
+
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardwidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappybird");
+        frame.setVisible(true);
+        frame.setSize(boardwidth , boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Flappybird flappybird = new Flappybird();
+        frame.add(flappybird);
+        frame.pack();
+        frame.setVisible(true);
+        
+
+
+
+        
+
+        
+    }
+}
---
+++ src/Flappybird.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import java.swing.*;
+
+public class Flappybird extends JPanel{
+
+    int boardwidth = 360;
+    int boardHeight = 640;
+
+    
+
+    Image backgroundImag;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+
+
+    Flappybird(){
+        setPreferredSize(new Dimension(boardwidth, boardHeight));
+        setBackground(Color.blue);
+
+        backgroundImag = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        
+    }
+        
+
+    
+}
